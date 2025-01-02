7533b2e436ae7b6a7b7d1eac1629e05b22433a20
dhtan <dinghangtan@gmail.com>
2025-01-02 17:40:37 +0800

    add function `tdh`

--- src/SayHello.jl
+++ src/SayHello.jl
@@ -1,9 +1,14 @@
 module SayHello
 
 export sayhello
+export tdh
 # Write your package code here.
 function sayhello()
     @info "Hello, world!, I am dhtan"
+end
+
+function tdh()
+    @info "Hello, world!, I am tdh"
 end
 
 
